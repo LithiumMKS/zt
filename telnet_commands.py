4c328a102acdd733b84c443d741ab8098189ba55
Maks Sandakov <sandakov.maks@gmail.com>
2022-08-18 10:57:33 +0500

    добавил комментарии

--- telnet_commands.py
+++ telnet_commands.py
@@ -1,21 +1,25 @@
 import telnetlib
 import time
 from datetime import datetime
-start_time = datetime.now()
 
-def to_bytes(line):
-    return f"{line}\r\n".encode("utf-8")
-
-command_text = '''
+start_time = datetime.now() ##обозначает время начала выполнения программы
+command_text = ''' 
+conf
+spanning-tree pathcost method long
+end
 copy run start
 y
 '''
 
-switch_list = []
-command_list = command_text.split('\n')
+switch_list = [] ##пустой список коммутаторов, который в дальнейшем будет заполняться и обрабатываться
+command_list = command_text.split('\n') ##список команд, составленный путем разделения текста на строки
+
+def to_bytes(line): ##добавляет в строку знаки переноса и возврата каретки и преобразует в байтовое значение для чтения telnetlib
+    return f"{line}\r\n".encode("utf-8")
+
 def telnet_commands():
     for switch in switch_list:
-        telnet = telnetlib.Telnet(switch)
+        telnet = telnetlib.Telnet(switch, timeout=1)
         telnet.read_until(b'User Name')
         telnet.write(b'ztbot\n')
         telnet.read_until(b'Password')
@@ -25,11 +29,11 @@ def telnet_commands():
             telnet.write(to_bytes(command))
         telnet.close()
 
-with open('switches.txt', 'r') as f:
+with open('switches.txt', 'r') as f: ##считывает ip из файла и заполняет ими список switch_list
     lines = f.readlines()
     for line in lines:
         line = line.strip()
         switch_list.append(line)
 
 telnet_commands()
-print(datetime.now() - start_time)
+print(datetime.now() - start_time) ##считает время выполнения программы
