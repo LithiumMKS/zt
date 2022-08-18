ed1ee86843f65f438e29a7716b6f068d9704a75c
Maks Sandakov <sandakov.maks@gmail.com>
2022-08-18 12:48:32 +0500

    добавил обработку исключений по таймауту соединения

--- telnet_commands.py
+++ telnet_commands.py
@@ -1,6 +1,7 @@
 import telnetlib
 import time
 from datetime import datetime
+import socket
 
 start_time = datetime.now() ##обозначает время начала выполнения программы
 command_text = ''' 
@@ -19,16 +20,20 @@ def to_bytes(line): ##добавляет в строку знаки перено
 
 def telnet_commands():
     for switch in switch_list:
-        telnet = telnetlib.Telnet(switch, timeout=1)
-        telnet.read_until(b'User Name')
-        telnet.write(b'ztbot\n')
-        telnet.read_until(b'Password')
-        telnet.write(b'greenpointbot\n')
-        telnet.read_until(b'#')
-        for command in command_list:
-            telnet.write(to_bytes(command))
-        telnet.close()
-
+       # time.sleep(0.5)
+        try:
+            telnet = telnetlib.Telnet(switch, timeout=20)
+            telnet.set_debuglevel(2)
+            telnet.read_until(b'User Name', timeout=10)
+            telnet.write(b'ztbot\n')
+            telnet.read_until(b'Password', timeout=10)
+            telnet.write(b'greenpointbot\n')
+            telnet.read_until(b'#', timeout=10)
+            for command in command_list:
+                telnet.write(to_bytes(command))
+            #telnet.close()
+        except socket.timeout: ##позволяет продолжить выполнение, если хост не отвечает по таймауту
+            print("connection time out caught")
 with open('switches.txt', 'r') as f: ##считывает ip из файла и заполняет ими список switch_list
     lines = f.readlines()
     for line in lines:
