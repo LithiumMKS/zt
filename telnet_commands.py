7bd518f32050801510d753591934a6dfc0672e21
Maks Sandakov <sandakov.maks@gmail.com>
2022-08-18 16:37:15 +0500

    добавил скачивание файла со списком оборудования из гв, временно закомментил выполнение телнета

--- telnet_commands.py
+++ telnet_commands.py
@@ -2,18 +2,20 @@ import telnetlib
 import time
 from datetime import datetime
 import socket
+import requests
 
 start_time = datetime.now() ##обозначает время начала выполнения программы
-command_text = ''' 
-conf
-spanning-tree pathcost method long
-end
+url = 'http://ufa.groupw.ru/plugins/switch/config/5/switch.cfg'
+r = requests.get(url, allow_redirects=True)
+open('gw_switch.cfg', 'wb').write(r.content) ##запись в файл списка оборудования из гв
+alc6224_command_text = ''' 
 copy run start
 y
 '''
 
 switch_list = [] ##пустой список коммутаторов, который в дальнейшем будет заполняться и обрабатываться
-command_list = command_text.split('\n') ##список команд, составленный путем разделения текста на строки
+command_list = alc6224_command_text.split('\n') ##список команд, составленный путем разделения текста на строки
+
 
 def to_bytes(line): ##добавляет в строку знаки переноса и возврата каретки и преобразует в байтовое значение для чтения telnetlib
     return f"{line}\r\n".encode("utf-8")
@@ -34,11 +36,21 @@ def telnet_commands():
             #telnet.close()
         except socket.timeout: ##позволяет продолжить выполнение, если хост не отвечает по таймауту
             print("connection time out caught")
+
+
 with open('switches.txt', 'r') as f: ##считывает ip из файла и заполняет ими список switch_list
     lines = f.readlines()
     for line in lines:
         line = line.strip()
         switch_list.append(line)
 
-telnet_commands()
+#telnet_commands()
+
+
+
+
+
+
+
+
 print(datetime.now() - start_time) ##считает время выполнения программы
